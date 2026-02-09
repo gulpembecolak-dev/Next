6eb1251f556aa3f9975cab84e05c8d9d226fc77b
gulpembe colak <gulpembecolak@gmail.com>
2026-02-09 15:16:10 +0100

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repo = "next01";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
